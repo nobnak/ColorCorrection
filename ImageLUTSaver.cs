8bcd598ff528e3f5fc37796fd5b093ba04ea9891
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-09-10 13:54:25 +0900

    LUT を画像ファイルとして保存する機能

---
+++ ImageLUTSaver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Gist;
+
+namespace ColorCorrection {
+    [ExecuteInEditMode]
+    public class ImageLUTSaver : MonoBehaviour {
+        public System.Environment.SpecialFolder folderEnum = System.Environment.SpecialFolder.MyDocuments;
+        public string filename = "LUT.png";
+
+        protected Texture2D buffer;
+
+        #region Unity
+        void OnDisable() {
+            ObjectDestructor.Destroy (buffer);
+        }
+        #endregion
+
+        public void Save(LUT3D lut) {
+            if (lut == null)
+                return;
+
+            var tex = GetBuffer ();
+            lut.ConvertBack (tex);
+            File.WriteAllBytes (GetPath (), tex.EncodeToPNG ());
+        }
+
+        public string GetFolder() {
+            return System.Environment.GetFolderPath (folderEnum);
+        }
+        public string GetPath() {
+            return Path.Combine (GetFolder (), filename);
+        }
+        public Texture2D GetBuffer() {
+            if (buffer == null)
+                buffer = new Texture2D (4, 4, TextureFormat.ARGB32, false, true);
+            return buffer;
+        }
+    }
+}
